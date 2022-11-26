285f277341ea61ad733b608a4505ac72225047cf
김영훈 <joseph0926@naver.com>
2022-11-26 11:05:20 +0900

    2022.11.26

--- start_project/src/App.js
+++ start_project/src/App.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import HomePage from "./shared/pages/HomePage";
+import Navbar from "./shared/components/Navbar";
+import AuthPage from "./user/pages/AuthPage";
+import BoardPage from "./board/pages/BoardPage";
+import SingleBoardPage from "./board/pages/SingleBoardPage";
+import CommunityPage from "./community/pages/CommunityPage";
+import SingleCommunityPage from "./community/pages/SingleCommunityPage";
+import ErrorPage from "./shared/pages/ErrorPage";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <BrowserRouter>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<HomePage></HomePage>}></Route>
+        <Route path="auth" element={<AuthPage></AuthPage>}></Route>
+        <Route path="board" element={<BoardPage></BoardPage>}></Route>
+        <Route
+          path="board/:id"
+          element={<SingleBoardPage></SingleBoardPage>}
+        ></Route>
+        <Route
+          path="community"
+          element={<CommunityPage></CommunityPage>}
+        ></Route>
+        <Route
+          path="community/:id"
+          element={<SingleCommunityPage></SingleCommunityPage>}
+        ></Route>
+        <Route path="*" element={<ErrorPage></ErrorPage>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
---
+++ start_project/src/shared/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+
+import Hero from "../components/Hero";
+
+const HomePage = () => {
+  return (
+    <Fragment>
+      <Hero></Hero>
+    </Fragment>
+  );
+};
+
+export default HomePage;
